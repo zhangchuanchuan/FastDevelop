5e439a402baba463c9aa25e01e382ad18695d2d1
zhangchuanchuan <zhangchuanchuan@58ganji.com>
2016-08-31 20:05:06 +0800

    建立recycler相关的两个common类

---
+++ app/src/main/java/com/stream/fastdevelop/common/recycleradapter/CommonRecyclerAdapter.java
@@ -0,0 +1,38 @@
+package com.stream.fastdevelop.common.recycleradapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * description：
+ * ===============================
+ * creator：张川川
+ * create time：2016/8/31 17:52
+ * ===============================
+ * reasons for modification：
+ * Modifier：
+ * Modify time：
+ */
+public class CommonRecyclerAdapter extends RecyclerView.Adapter<RecyclerViewCommonViewHolder>{
+
+
+
+    public CommonRecyclerAdapter(){
+
+    }
+
+    @Override
+    public RecyclerViewCommonViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerViewCommonViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/stream/fastdevelop/common/recycleradapter/RecyclerViewCommonViewHolder.java
@@ -0,0 +1,28 @@
+package com.stream.fastdevelop.common.recycleradapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * description：
+ * ===============================
+ * creator：58
+ * create time：2016/8/31 18:01
+ * ===============================
+ * reasons for modification：
+ * Modifier：
+ * Modify time：
+ */
+public class RecyclerViewCommonViewHolder extends RecyclerView.ViewHolder {
+
+    public RecyclerViewCommonViewHolder(View itemView) {
+        super(itemView);
+    }
+
+
+//    public static RecyclerViewCommonViewHolder getViewHolder(){
+//
+//    }
+
+
+}
